9a1b829b8ab7431f666f74bc094cf662dd18e680
Tharunfitfun <tharunfitfun204@gmail.com>
2025-07-05 12:58:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,6 +53,8 @@ class Bot(Client):
         now = datetime.now(tz)
         time = now.strftime("%H:%M:%S %p")
         await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+except Exception as e:
+    print(f"[LOG_CHANNEL ERROR] Cannot send to log channel: {e}")
         await self.send_message(chat_id=SUPPORT_CHAT_ID, text=script.RESTART_GC_TXT.format(today, time))
         app = web.AppRunner(await web_server())
         await app.setup()
